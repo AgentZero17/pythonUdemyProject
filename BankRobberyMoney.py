acd82f9570f4d0df8781c6760d0ae48897a7a976
AgentZero17 <48912650+AgentZero17@users.noreply.github.com>
2022-08-02 01:46:31 -0500

    tuesday 146

---
+++ BankRobberyMoney.py
@@ -0,0 +1,3 @@
+moneyInTheBank = 19867324678987.99
+
+print(moneyInTheBank/5)
---
+++ hello.py
@@ -0,0 +1,17 @@
+print("Hello")
+
+x = 100
+mother_of_dragons = 1
+
+print(x + mother_of_dragons)
+
+
+one, two, three = 1, 2, 3
+print()
+print(1)
+print()
+print(2)
+print() 
+print(3)
+
+print(one.bit_length)
